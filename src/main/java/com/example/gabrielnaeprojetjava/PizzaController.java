cbf553e0d5f1e6df01af830d892db0f95b66c7bd
Raider472 <gabrielnae1@gmail.com>
2023-06-08 15:51:19 +0200

    verification Saisie

--- src/main/java/com/example/gabrielnaeprojetjava/PizzaController.java
+++ src/main/java/com/example/gabrielnaeprojetjava/PizzaController.java
@@ -3,10 +3,7 @@ package com.example.gabrielnaeprojetjava;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
@@ -30,6 +27,9 @@ public class PizzaController implements Initializable {
             tablePizza.setItems(pizzaTable);
         });
     }
+
+    @FXML
+    private Label labelErreur;
 
     @FXML
     private TableView tablePizza;
@@ -59,7 +59,45 @@ public class PizzaController implements Initializable {
     private Button buttonSuprimer;
     @FXML
     private void ajouterPizzaBouton() {
-        this.ajouterPizzaDansTab(this.textRecette.getText(), Float.parseFloat(this.textPrix.getText()), Integer.parseInt(this.textNbIngredients.getText()));
+        String nom = textRecette.getText();
+        String prixText = textPrix.getText();
+        String nbIngredientsText = textNbIngredients.getText();
+
+        if(nom.isEmpty()) {
+            labelErreur.setText("Veulliez mettre un nom");
+            labelErreur.setVisible(true);
+        }
+        else if (prixText.isEmpty() || !isFloat(prixText)) {
+            labelErreur.setText("Veuillez entrer un prix valide");
+            labelErreur.setVisible(true);
+        }
+        else if (nbIngredientsText.isEmpty() || !isInteger(nbIngredientsText)) {
+            labelErreur.setText("Veuillez entrer un nombre d'ingrédients valide");
+            labelErreur.setVisible(true);
+        }
+        else {
+            labelErreur.setVisible(false);
+            labelErreur.setText("");
+            this.ajouterPizzaDansTab(this.textRecette.getText(), Float.parseFloat(this.textPrix.getText()), Integer.parseInt(this.textNbIngredients.getText()));
+        }
+    }
+
+    private boolean isFloat(String value) {
+        try {
+            Float.parseFloat(value);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private boolean isInteger(String value) {
+        try {
+            Integer.parseInt(value);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
     @FXML
     private void supprimerTouteLesPizzasBouton() {
